e7c8dcda45896f47de1af04f6dc92fe83b36c903
JK <speedjjk@gmail.com>
2018-07-02 19:19:59 +0900

    Test

---
+++ AlgoTest/src/test/test/level_1/TestSample1.java
@@ -0,0 +1,20 @@
+package test.level_1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestSample1 {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		
+		fail("Not yet implemented");
+	}
+
+}
